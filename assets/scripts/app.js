3ca852e694ca2564355b3fb03df312b5d0081abf
xiamjf <xiamjf@gmail.com>
2018-12-17 10:06:34 -0500

    Changed require folders for app.js and unhid upload li
    That^

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,12 +6,13 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-const events = require('./auth/events.js')
+const authEvents = require('./auth/events.js')
+const picEvents = require('./pictures/events.js')
 
 $(() => {
-  $('#multipart-form-data').on('submit', events.onFormSubmit)
-  $('#sign-up').on('submit', events.onSignUp)
-  $('#sign-in').on('submit', events.onSignIn)
+  $('#multipart-form-data').on('submit', picEvents.onFormSubmit)
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
   // Hide/show animation hamburger function
   $('.navbar-toggler').on('click', function () {
   // Take this line to first hamburger animations
