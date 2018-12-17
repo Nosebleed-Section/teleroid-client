8cdc6b426e8519b12a94b4363d2bc641a93853e2
joelthibodeau <joel.thibodeau@gmail.com>
2018-12-17 16:17:09 -0500

    Fix ui to properly display page numbers as links/not-links with Shawn.
    Additional:
    - center pictures collection on page in html
    - center page numbers with css class

--- assets/scripts/pictures/ui.js
+++ assets/scripts/pictures/ui.js
@@ -8,34 +8,35 @@ const onGetAllPicturesSuccess = function (data) {
   store.pictures = data.pictures
   store.picsPerPage = 12
   store.pageNums = (data.pictures.length % store.picsPerPage) + 1
+  console.log(store.pageNums)
 }
 
 const displayPageOfPictures = (page) => {
-  console.log(store.pictures)
   const i = page * store.picsPerPage
-  console.log(store.pictures[i])
   const gridrow = [
     [store.pictures[i], store.pictures[i + 1], store.pictures[i + 2]],
     [store.pictures[i + 3], store.pictures[i + 4], store.pictures[i + 5]],
     [store.pictures[i + 6], store.pictures[i + 7], store.pictures[i + 8]],
-    [store.pictures[i + 9], store.pictures[i + 10], store.pictures[i + 12]]
+    [store.pictures[i + 9], store.pictures[i + 10], store.pictures[i + 11]]
   ]
-  let html = allPicturesTemplate({ gridrow: gridrow })
+  const html = allPicturesTemplate({ gridrow: gridrow })
+  let moreHtml = ''
   if (store.pageNums > 1) {
-    html += '<div class="page-number-container">'
-    for (let i = 0; i < store.pageNums; i++) {
+    moreHtml += `<div class="page-number-container">`
+    for (let i = 0; i <= store.pageNums; i++) {
       if (i !== page) {
-        html += `<div class="page-number"><a href="#" id="${i}">${i + 1}</a></div>`
+        moreHtml += `<div class="page-number"><a href="#" id="${i}">${i + 1}</a></div>`
       } else {
-        html += `<div>${i + 1}</div>`
+        moreHtml += `<div>${i + 1}</div>`
       }
     }
+    moreHtml += `</div>`
   }
-  html += '</div>'
   $('.div-of-divs').html(html)
+  $('.div-of-divs').append(moreHtml)
   $('.page-number').on('click', function (event) {
     event.preventDefault()
-    displayPageOfPictures(event.target.id)
+    displayPageOfPictures(parseInt(event.target.id))
   })
 }
 
